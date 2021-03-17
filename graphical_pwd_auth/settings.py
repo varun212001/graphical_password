ac8beadebe6aaa16398c011e8212c764ed2dc844
Varun Kumar M <53909502+varun212001@users.noreply.github.com>
2021-03-17 20:43:49 +0530

    Update settings.py

--- graphical_pwd_auth/settings.py
+++ graphical_pwd_auth/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'varund2e@gmail.com'
-EMAIL_HOST_PASSWORD = 'nandhatech2020'
+EMAIL_HOST_USER = 'abc@gmail.com'
+EMAIL_HOST_PASSWORD = '******'
